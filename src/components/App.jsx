290202a37107abfb8e7127041e51a7fcd62f9ac5
Maksym-Tykhonenko <maksym.tykhonenko88@gmail.com>
2023-04-29 10:25:55 +0300

    starting fitch

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,53 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template!!!
-    </div>
-  );
+import { Component } from "react";
+
+export class App extends Component {
+
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  };
+
+  handleLiaveGoodFeedback = () => {
+    this.setState((prevState) => {
+      return {
+        good: prevState.good + 1,
+      }
+    })
+  };
+  handleLiaveNeutralFeedback = () => {
+    this.setState((prevState) => {
+      return {
+        neutral: prevState.neutral + 1,
+      }
+    })
+  };
+  handleLiaveBadFeedback = () => {
+    this.setState((prevState) => {
+      return {
+        bad: prevState.bad + 1,
+      }
+    })
+  };
+
+  render() {
+    return (
+      <div>
+        <div>
+          <p>Please leave feedback</p>
+          <button type="button" onClick={this.handleLiaveGoodFeedback}>Good</button>
+          <button type="button" onClick={this.handleLiaveNeutralFeedback}>Neutral</button>
+          <button type="button" onClick={this.handleLiaveBadFeedback}>Bad</button>
+        </div>
+        <div>
+          <p>Statistics</p>
+          <p>Good: {this.state.good }</p>
+          <p>Neutral: {this.state.neutral }</p>
+          <p>Bad: {this.state.bad }</p>
+        </div>
+
+      </div>
+    );
+  };
 };
+

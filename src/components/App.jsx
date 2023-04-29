387b86674e77b68452cd108d41c5752897f0d878
Maksym-Tykhonenko <maksym.tykhonenko88@gmail.com>
2023-04-29 14:34:08 +0300

    add oll fitch

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,53 +1,64 @@
 import { Component } from "react";
 
 export class App extends Component {
-
   state = {
-    good: 0,
-    neutral: 0,
-    bad: 0
+  good: 0,
+  neutral: 0,
+  bad: 0
+  };
+  
+  handleBtnClick (option) {
+    //console.log('click on ', { option })
+    this.setState((prevSt) => {
+      return {
+        [option]: prevSt[option] + 1,
+        
+      }
+    })
   };
 
-  handleLiaveGoodFeedback = () => {
-    this.setState((prevState) => {
-      return {
-        good: prevState.good + 1,
-      }
-    })
+  countTotalFeedback() {
+    const {good, neutral, bad} = this.state;
+    return  good + neutral + bad;
   };
-  handleLiaveNeutralFeedback = () => {
-    this.setState((prevState) => {
-      return {
-        neutral: prevState.neutral + 1,
-      }
-    })
-  };
-  handleLiaveBadFeedback = () => {
-    this.setState((prevState) => {
-      return {
-        bad: prevState.bad + 1,
-      }
-    })
-  };
+
+  countPositiveFeedbackPercentage() {
+    // % позитивних оцінок = (кількість "good" оцінок / загальна кількість оцінок) * 100%
+    const { good } = this.state;
+    return (good / this.countTotalFeedback()) * 100
+    
+  }
 
   render() {
+    const options = Object.keys(this.state)
+    const total = this.countTotalFeedback()
+    const positivePers = this.countPositiveFeedbackPercentage();
+    console.log(positivePers)
     return (
-      <div>
+      <>
+        <p>Please leave feedback</p>
         <div>
-          <p>Please leave feedback</p>
-          <button type="button" onClick={this.handleLiaveGoodFeedback}>Good</button>
-          <button type="button" onClick={this.handleLiaveNeutralFeedback}>Neutral</button>
-          <button type="button" onClick={this.handleLiaveBadFeedback}>Bad</button>
-        </div>
-        <div>
-          <p>Statistics</p>
-          <p>Good: {this.state.good }</p>
-          <p>Neutral: {this.state.neutral }</p>
-          <p>Bad: {this.state.bad }</p>
+          {options.map((option, index) => {
+            return (
+              <button 
+                type="button" 
+                key={index}
+                onClick={()=>{this.handleBtnClick(option)} }>{option}</button>
+            )
+          })}
         </div>
 
-      </div>
-    );
-  };
+        <p>Statistics</p>
+        <div>
+          <p>Good: {this.state.good }</p>
+          <p>Neutral: {this.state.neutral }</p>
+          <p>Bad: {this.state.bad}</p>
+          {total > 0 && <div><p>Total: {total }</p><p>positivePers: {positivePers }%</p></div>}
+          
+        </div>
+      </>
+    )
+   }
+
 };
 
